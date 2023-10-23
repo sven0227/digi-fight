ad2c6b5e4cf5c97df1162d0909d84ebb1c807526
Sven <124936934+sven0227@users.noreply.github.com>
2023-10-23 13:36:41 +0900

    show footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import ChatBox from './ChatBox'
 function Footer() {
   return (
     <>
-      <footer className='fixed flex justify-end items-end'>
+      <footer className='fixed flex justify-end items-end bottom-4 right-4'>
         <ChatBox />
       </footer>
     </>
